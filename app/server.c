d7c9f0e88f32f173a5f727c938df5011f5feb9e7
primequantuM4 <gediezra@gmail.com>
2024-05-22 05:26:45 +0300

    codecrafters submit [skip ci]

--- app/server.c
+++ app/server.c
@@ -8,7 +8,12 @@
 #include <unistd.h>
 
 #define BUFFER_SIZE 4096
-char *response();
+
+char *response(char *request_target);
+char *echo_response(char *request_target);
+
+char **split_tokens(char *buff);
+
 int main() {
   // Disable output buffering
   setbuf(stdout, NULL);
@@ -75,19 +80,80 @@ int main() {
   buffer[recieved_buff] = '\0';
 
   char *parse = strtok(buffer, "\n");
-  char* request = strtok(parse, " ");
+  // char* request = strtok(parse, " ");
 
-  request = strtok(NULL, " ");
+  // request = strtok(NULL, " ");
+  char *echo_res = echo_response(parse);
 
-  char *empty_response = response(request);
-  send(client_socket_fd, empty_response, strlen(empty_response), 0);
+  // char *empty_response = response(request);
+  send(client_socket_fd, echo_res, strlen(echo_res), 0);
   close(server_fd);
 
   return 0;
 }
 
-char *response(char* request_target) {
-    if(strcmp(request_target, "/") == 0){ return "HTTP/1.1 200 OK\r\n\r\n";  }
-    else { return "HTTP/1.1 404 Not Found\r\n\r\n"; }
-    
+char *response(char *request_target) {
+  if (strcmp(request_target, "/") == 0) {
+    return "HTTP/1.1 200 OK\r\n\r\n";
+  } else {
+    return "HTTP/1.1 404 Not Found\r\n\r\n";
+  }
+}
+
+char *echo_response(char *request_target) {
+  char **split_buff = split_tokens(request_target);
+  const char *slash = "/";
+  const char *echo = "echo";
+
+  if (strcmp(split_buff[1], slash) == 0)
+    return response(split_buff[1]);
+
+  char *echo_word = strtok(split_buff[1], "/");
+  if (strcmp(echo_word, echo) != 0) {
+    printf("I have reached here\n");
+    return response(split_buff[1]);
+  }
+
+  echo_word = strtok(NULL, "/");
+
+  int content_length = strlen(echo_word);
+  int buffer_size = snprintf(NULL, 0,
+                             "HTTP/1.1 200 OK\r\nContent-Type: "
+                             "text/plain\r\nContent-Length: %d\r\n\r\n%s",
+                             content_length, echo_word);
+
+  char *buffer = malloc(buffer_size + 1);
+
+  sprintf(buffer,
+          "HTTP/1.1 200 OK\r\n"
+          "Content-Type: text/plain\r\n"
+          "Content-Length: %d\r\n\r\n%s",
+          content_length, echo_word);
+
+  return buffer;
+}
+
+char **split_tokens(char *request_target) {
+  int index = 0;
+  for (int i = 0; i < strlen(request_target); i++) {
+    if (request_target[i] == ' ') {
+      index++;
+    }
+  }
+  index++;
+
+  if (index < 3) {
+    return NULL;
+  }
+  char **buff_arr = malloc((index + 1) * sizeof(char *));
+
+  char *token = strtok(request_target, " ");
+  for (int i = 0; i < index; i++) {
+    buff_arr[i] = malloc(strlen(token) + 1);
+    strcpy(buff_arr[i], token);
+    printf("This is the current token at [%d]: %s\n", i, buff_arr[i]);
+    token = strtok(NULL, " ");
+  }
+
+  return buff_arr;
 }
